5f4ab5e7201b170a02704d1df24896c0e5a898ca
mbk97 <oyindamola850@gmail.com>
2025-07-07 15:01:04 +0100

    update

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
@@ -7,88 +8,121 @@ class Menu extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<Map<String, dynamic>> menuItemsOne = [
       {'image': 'assets/images/transfer.png', 'title': 'Send Money'},
-      {'image': 'assets/images/top-up.png', 'title': 'Top-up Walleet'},
-      {'image': 'assets/images/wallet.svg', 'title': 'Bill Payment'},
-      {'image': 'assets/images/scan.png', 'title': 'Code QR'},
+      {'image': 'assets/images/top-up.png', 'title': 'Top-up Wallet'},
+      {'image': 'assets/images/Wallet.svg', 'title': 'Bill Payment'},
+      {'image': 'assets/images/Scan2.png', 'title': 'Code QR'},
     ];
 
     return Scaffold(
       backgroundColor: const Color(0XFF09703E),
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFFFFFF),
-                    shape: BoxShape.rectangle,
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(
-                      Icons.arrow_forward,
-                      size: 25,
-                      color: Color(0xFF09703E),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 50),
-                const Text(
-                  'Menu',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 30),
-            Expanded(
-              child: ListView.builder(
-                itemCount: menuItemsOne.length,
-                itemBuilder: (context, index) {
-                  final item = menuItemsOne[index];
-                  final String imgPath = item['image'];
-                  final bool isSvg = imgPath.toLowerCase().endsWith('.svg');
-
-                  return Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 10,
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          menuItemsOne[index]['image'],
-                          width: 40,
-                          height: 40,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 40,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
                         ),
-                        const SizedBox(width: 20),
-                        Text(
-                          menuItemsOne[index]['title'],
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            size: 25,
+                            color: Color(0xFF09703E),
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                  );
-                },
+                    const Center(
+                      child: Text(
+                        'Menu',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+
+              const SizedBox(height: 30),
+              const Text(
+                "Shortcuts",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: menuItemsOne.length,
+                  itemBuilder: (context, index) {
+                    final item = menuItemsOne[index];
+                    final String imgPath = item['image'];
+                    final bool isSvg = imgPath.toLowerCase().endsWith('.svg');
+
+                    return Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      child: ListTile(
+                        contentPadding: EdgeInsets.zero, // Align left
+                        leading: Container(
+                          height: 50,
+                          width: 50,
+                          padding: const EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            color: const Color(0XFFE9FFAB),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: isSvg
+                              ? SvgPicture.asset(imgPath, fit: BoxFit.contain)
+                              : Image.asset(imgPath, fit: BoxFit.contain),
+                        ),
+                        title: Text(
+                          item["title"],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        ),
+                        trailing: const Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                          size: 12,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              Divider(
+                color: Colors.white.withAlpha(77),
+                thickness: 1.5,
+                indent: 16.0,
+                endIndent: 16.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
